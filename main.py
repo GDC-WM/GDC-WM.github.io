66b97c03f8c8c99380503c0860df204052024c0c
probably-nothing <5199272+probably-nothing@users.noreply.github.com>
2021-04-10 03:47:29 -0400

    Add parryt page

--- main.py
+++ main.py
@@ -8,6 +8,9 @@ site = Flask(__name__)
 def home():
     return render_template("home.html")
 
+@site.route("/parryt")
+def parryt():
+	return render_template("parryt.html")
 
 if __name__ == "__main__":
     site.run(debug = True)
